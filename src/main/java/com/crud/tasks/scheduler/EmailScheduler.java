68be9b92d244baf85e45b3d97d56d09242b6508a
Katarzyna Kropiak <k.kropiak@gmail.com>
2022-06-28 15:06:35 +0200

    E-mail Scheduler

---
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -0,0 +1,44 @@
+package com.crud.tasks.scheduler;
+
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.service.SimpleEmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class EmailScheduler {
+
+    private final SimpleEmailService simpleEmailService;
+    private final TaskRepository taskRepository;
+    private final AdminConfig adminConfig;
+
+    private static final String SUBJECT = "Tasks: Once a day email";
+
+    @Scheduled(cron = "0 0 10 * * *")
+    public void sendInformationEmail() {
+        long size = taskRepository.count();
+        if (size == 1L) {
+            simpleEmailService.send(
+                    Mail.builder()
+                            .mailTo(adminConfig.getAdminMail())
+                            .subject(SUBJECT)
+                            .message("Currently in database you got: " + size + " task")
+                            .toCc(null)
+                            .build()
+            );
+        } else {
+            simpleEmailService.send(
+                    Mail.builder()
+                            .mailTo(adminConfig.getAdminMail())
+                            .subject(SUBJECT)
+                            .message("Currently in database you got: " + size + " tasks")
+                            .toCc(null)
+                            .build()
+            );
+        }
+    }
+}
