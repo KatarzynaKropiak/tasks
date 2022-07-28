4e3bfd1fff68be28ddcedca5d17719754be8f24f
Katarzyna Kropiak <k.kropiak@gmail.com>
2022-07-28 16:14:21 +0200

    32.2 E-mail template

---
+++ src/main/java/com/crud/tasks/repository/service/MailCreatorService.java
@@ -0,0 +1,38 @@
+package com.crud.tasks.repository.service;
+
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.config.CompanyConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@Service
+public class MailCreatorService {
+
+    @Autowired
+    private AdminConfig adminConfig;
+
+    @Autowired
+    private CompanyConfig companyConfig;
+
+    @Autowired
+    @Qualifier("templateEngine")
+    private TemplateEngine templateEngine;
+
+
+    public String buildTrelloCardEmail(String message) {
+        Context context = new Context();
+        context.setVariable("message", message);
+        context.setVariable("tasks_url", "https://katarzynakropiak.github.io/");
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("goodbye_message", "Have a great day :)");
+        context.setVariable("companyName", companyConfig.getCompanyName());
+        context.setVariable("companyEmail", companyConfig.getCompanyEmail());
+        context.setVariable("companyPhone", companyConfig.getCompanyPhone());
+        return templateEngine.process("mail/created-trello-card-mail", context);
+    }
+
+}
